1c1b054dbe8eca6acd6b16e2230b5f24d31cce4b
NgocPT <ngocpt@bkav.com>
2020-08-08 21:53:23 +0700

    Clear server

--- server/next.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-	console.log('Haha')
-  res.header('X-Hello', 'World')
-  next()
-  return;
-}
--- server/server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-json-server --watch db.json --routes routes.json --static ./public/ --middlewares hello.js
