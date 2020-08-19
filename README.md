69b0854f181584a1bde45cc9e399f08c6ffcde83
NgocPT <ngocpt@bkav.com>
2020-08-19 07:47:21 +0700

    Todo lists.

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
 # Bắt đầu dự án blog.
 # Một cách để học lập trình VueJS
+# todo
 
 # Server Falcon
+## Project setup
+```
+yarn install
+```
 
 # DB PostgreSQL
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
 
 # Web VueJS
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <h4 class="bg-primary text-white text-center p-2">
+     {{name}}'s To Do List
+    </h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Task</div>
+        <div class="col-2 font-weight-bold">Done</div>
+      </div>
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+        <div class="col">{{t.action}}</div>
+        <div class="col-2 text-center">
+          <input type="checkbox" v-model="t.done" class="form-check-input"/>
+          {{t.done}}
+        </div>
+      </div>
+      <div class="row py-2">
+        <div class="col">
+          <input class="form-control" v-model='newItemText'/>
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox" class="form-check-input" v-model="hideCompleted"/>
+          <label class="form-check-label font-weight-bold">
+            Hide completed tasks
+          </label>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: function() {
+    return {
+      name: 'Adam',
+      hideCompleted: false,
+      tasks: [
+      {action: 'Hello', done: false},
+      {action: 'Haha', done: false},
+      {action: 'Lala', done: false},
+      {action: 'Gaga', done: false},
+      {action: 'Mama', done: false}],
+      newItemText: ''
+    }
+  },
+  computed: {
+    filteredTasks: function() {
+      return this.hideCompleted ?
+        this.tasks.filter(t => !t.done) : this.tasks
+    }
+  },
+  methods: {
+    addNewTodo: function() {
+      this.tasks.push({
+        action: this.newItemText, done: false
+      })
+      this.newItemText = ''
+    }
+  }
+}
+</script>
