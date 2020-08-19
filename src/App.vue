4425e472a3de5af1720a5b594cc42fd9fbda96b1
NgocPT <ngocpt@bkav.com>
2020-08-20 00:30:26 +0700

    End Hello.

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,25 @@
      {{name}}'s To Do List
     </h4>
     <div class="container-fluid p-4">
-      <div class="row">
-        <div class="col font-weight-bold">Task</div>
-        <div class="col-2 font-weight-bold">Done</div>
+      <div class="row" v-if="filteredTasks.length == 0">
+        <b>Nothing todo. Hurrah!</b>
       </div>
-      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
-        <div class="col">{{t.action}}</div>
-        <div class="col-2 text-center">
-          <input type="checkbox" v-model="t.done" class="form-check-input"/>
-          {{t.done}}
+      <template v-else>
+        <div class="row">
+          <div class="col font-weight-bold">Task</div>
+          <div class="col-2 font-weight-bold">Done</div>
         </div>
-      </div>
+        <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+          <div class="col">{{t.action}}</div>
+          <div class="col-2 text-center">
+            <input type="checkbox" v-model="t.done" class="form-check-input"/>
+            {{t.done}}
+          </div>
+        </div>
+      </template>
       <div class="row py-2">
         <div class="col">
-          <input class="form-control" v-model='newItemText'/>
+          <input class="form-control" v-model="newItemText"/>
         </div>
         <div class="col-2">
           <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
@@ -29,6 +34,11 @@
           <label class="form-check-label font-weight-bold">
             Hide completed tasks
           </label>
+        </div>
+        <div class="col text-center">
+          <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted">
+            Delete Completed
+          </button>
         </div>
       </div>
     </div>
@@ -43,12 +53,7 @@ export default {
     return {
       name: 'Adam',
       hideCompleted: false,
-      tasks: [
-      {action: 'Hello', done: false},
-      {action: 'Haha', done: false},
-      {action: 'Lala', done: false},
-      {action: 'Gaga', done: false},
-      {action: 'Mama', done: false}],
+      tasks: [],
       newItemText: ''
     }
   },
@@ -64,6 +69,20 @@ export default {
         action: this.newItemText, done: false
       })
       this.newItemText = ''
+      this.storeData()
+    },
+    storeData() {
+      localStorage.setItem('todo', JSON.stringify(this.tasks))
+    },
+    deleteCompleted() {
+      this.tasks = this.tasks.filter(t => !t.done)
+      this.storeData()
+    }
+  },
+  created: function() {
+    let data = localStorage.getItem('todo')
+    if(data != null) {
+      this.tasks = JSON.parse(data)
     }
   }
 }
